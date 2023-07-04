1c15520de86af73a1c10ed1f8253fc376d904313
Caio Pires <caiogodoi_92@hotmail.com>
2023-07-03 22:32:20 -0300

    chore: Instalar biblioteca Joi

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-import { Express } from 'express'
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
+import Joi from 'joi'
+
+// config
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const mongoClient = new MongoClient(process.env.BASE_URL)
+let db
+
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db))
+  .catch(err => console.log(err.message))
+
+// variaveis
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!')
+})
+
+app.post('/cadastro', (req, res) => {
+  db.collection('users').insertOne({
+    email: 'caio@caio',
+    password: '1234'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`servidor rodando na porta ${port}`)
+})
